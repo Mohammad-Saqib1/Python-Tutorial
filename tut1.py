fdde03a03d6e095b121132acb4eab079eebe1788
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-15 23:34:58 +0530

    Add files via upload

---
+++ tut1.py
@@ -0,0 +1,15 @@
+#comments,escape sequence and print
+#this is  a comment
+"""this is a multi line comment"""
+print("Hello I m an engineer","CS branch 2yr","AIET")
+print("SAQI'B")
+print("saqib\n")
+print("saqib\\n")
+print("saqib\t")
+print("saqib\\t")
+print("saqib\b")
+print("saqib\f")
+print("saqib\ooo")
+print("saqib\\")
+print("saqib\'")
+print("saqib\r")
