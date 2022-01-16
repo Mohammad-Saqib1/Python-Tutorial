634ce317ec439a0dc62e15995604a52d5f6b3c2e
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-16 23:30:41 +0530

    Add files via upload

---
+++ tut2.py
@@ -0,0 +1,16 @@
+#variables,datatypes and typecasting
+var1=56
+var2="32"
+var3="78"
+var4=80
+var5="Saqib"
+var6="Engineer"
+var7="100"
+var8="2000"
+print(int(var7)+int(var8))
+print(var1+int(var2))
+print(str(var1)+(var2))
+print(var2+var3)
+print(var5+var6)
+
+print(var1+var4)
