3c1bef5dc26aa1448f45cd0aa5b96ac95629c504
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-17 20:36:44 +0530

    Add files via upload

---
+++ tut3.py
@@ -0,0 +1,13 @@
+#slicing and other function
+mystr="Harry is a good boy"
+
+print(mystr[:])
+print(mystr[1:7])
+print(mystr[1:8:2])
+print(mystr.capitalize())
+print(mystr.lower())
+print(mystr.replace("is","are"))
+print(len(mystr))
+print(mystr.count("o"))
+print(mystr.endswith("boy"))
+print(mystr.endswith("good"))
