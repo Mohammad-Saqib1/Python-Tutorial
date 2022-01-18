5c39190b468c0761ea0218c59995df3dfcc5946b
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-18 23:34:15 +0530

    Add files via upload

---
+++ tut4.py
@@ -0,0 +1,22 @@
+
+# list and list function
+mob=["battery","charger","cablep in","display","touch","airfon","temper glass"]
+print(mob)
+print(mob[4])
+mob.pop()
+print(mob)
+
+mob.remove("touch")
+print(mob)
+
+mob.reverse()
+print(mob)
+
+mob.append("cover")
+print(mob)
+
+mob.insert(2,"laptop")
+print(mob)
+
+
+print(mob)
