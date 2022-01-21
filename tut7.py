df02e8a8e64524ef373c047a1dcd82d10d115256
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-21 22:41:50 +0530

    Add files via upload

---
+++ tut7.py
@@ -0,0 +1,22 @@
+#if,elif,else
+"""
+print("Enter your age")
+age=int(input())
+if age>18:
+    print("You can join the gym")
+elif age==18:
+    print("you can take a permission via parents then join")
+else:
+    print("You can't join the gym")
+        """
+print("Enter your age")
+age=int(input())
+if age>18 and age<90:
+    print("You can join the gym")
+elif age==18:
+    print("you can take a permission via parents then join")
+else:
+    print("You can't join the gym")
+
+    
+        
