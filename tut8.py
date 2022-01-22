1b1af281de5514322d5609ff463b02fd3a2acfcf
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-22 20:35:30 +0530

    Add files via upload

---
+++ tut8.py
@@ -0,0 +1,18 @@
+#loops
+"""
+list1=["harry","marry","carry","zarry"]
+
+for item in list1:
+    print(item)
+list2=[["saqib",1],["shakir",2],["shahir",3]] 
+for item in list2:
+   print(item)
+dict2=dict(list2)   
+for item,lollypop in dict2.items():
+    print(item,"lollypop is",lollypop)"""
+    #while loop
+i=0    
+while(i<45):
+    print(i)
+    i=i+1
+    
