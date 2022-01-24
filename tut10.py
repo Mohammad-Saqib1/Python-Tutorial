2c149e5715b5421d867c3a0982c76c433eed0e17
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-24 20:51:35 +0530

    Add files via upload

---
+++ tut10.py
@@ -0,0 +1,59 @@
+#operators
+#arithmetic operators
+print("5+6 is",5+6)
+print("5%6 is",5%6)
+print("5-6 is",5-6)
+print("5**6 is",5**6)
+print("5//6 is",5//6)
+print("5/6 is",5/6)
+print("5*6 is",5*6)
+#assigment operators
+print("assignment operatos")
+x=5
+x+=7
+print(x)
+
+x-=7
+print(x)
+
+x*=7
+print(x)
+
+x/=7
+print(x)
+
+x//=7
+print(x)
+
+x**=7
+print(x)
+
+x%=7
+print(x)
+#comparison operators
+#i==5
+#i=>5
+#i<=5
+#logical operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(a and b)
+print(b and a)
+print(b and b)
+print(b or a)
+#identity operators
+print(a is not b)
+print(a is b)
+print(a is a)
+print(b is a)
+print(b is b)
+#membership operators
+list=[2,3,4,5,6,22,35,56]
+print(32  in list)
+print(234  not in list)
+print(2  in list)
+#bitwise operators
+print(0 & 3)
+print(0 or 1)
