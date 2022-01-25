97329ba651b22830a1d83a7d2501814736774de3
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-01-25 22:55:41 +0530

    Add files via upload

---
+++ tut11.py
@@ -0,0 +1,6 @@
+#one line if else statement
+a=int(input("Enter a\n"))
+b=int(input("Enter b\n"))
+#if a>b:print("A is greater than B")
+print("A is greater tha B") if a>b else print("B is greater than A")
+    
