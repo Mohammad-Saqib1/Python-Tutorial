ec53bfce79f1b30a5a9cbd95a117180e40577780
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-02-05 20:38:23 +0530

    Add files via upload

---
+++ tut21.py
@@ -0,0 +1,12 @@
+#external and built module
+# print any random number b/w any number
+import random #built
+random_number=random.randint(10,20)
+print(random_number)
+#print any random number
+rand=random.random()*10
+print(rand)
+# print any random string
+lst=["star plus","DD1","Aaj tak","code with harry"]
+choice=random.choice(lst)
+print(choice)
