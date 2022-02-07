cb71446fdb2b52e28253d12511e54b45c0bc1966
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-02-07 19:41:17 +0530

    Add files via upload

---
+++ tut23.py
@@ -0,0 +1,17 @@
+# *args and **kwargs
+# print limited value or string
+def function_name(a,b,c,d):
+    print(a,b,c,d)
+#if youy want to print more and more value then use args and kwargs
+function_name("Harry","Saqib","Naman","Ranjan")
+def funargs(n,*args,**kwargs):
+    print(normal)
+    for item in args:
+        print(item)
+    for item,line in k
+    for key,value in kwargs.items():
+        print(key,value)
+har=["Hero","Zero","Mero","Bhago"]    
+normal="I am normal arguments"
+kw={"Harry":"Laddu","Saqib":"Jalebi","Naman":"Gulla","Ranjan":"Mithai"}
+funargs(normal,*har,**kw)
