d76e4634d6f0199e2c884de408647c29273bd176
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-02-16 23:39:47 +0530

    Add files via upload

---
+++ tut27.py
@@ -0,0 +1,6 @@
+ #import function
+ #This type accessor call all the function in file3
+import file3
+print(file3.add(7,7))
+#if you want to call only single function who made you in file3,then you make __main__ function in file3
+print(file3.add(6,6))
