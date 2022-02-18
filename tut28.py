7934377174062ddb7a07d73eac6ccd518011dfce
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-02-18 23:14:20 +0530

    Add files via upload

---
+++ tut28.py
@@ -0,0 +1,8 @@
+#join function
+lis=["Join","Cena","Roman","Dean","Seth","Khali","Shane"]
+for line in lis:
+    print(line,"and",end=" ")
+#if you want to fast done these work so you can use join()
+a=", ".join(lis)
+print("\nThese are the superstars of wwe")
+print(a)
