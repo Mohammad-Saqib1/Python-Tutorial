394904e0aeef43f6ed03f5c2050e3fb7017a1cea
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-02-20 22:59:57 +0530

    Add files via upload

---
+++ tut31.py
@@ -0,0 +1,20 @@
+#class concepts
+#code reusability
+class Student:
+    pass
+
+harry= Student()
+larry= Student()
+
+harry.name= "Harry"
+harry.std= 12
+harry.section= 1
+larry.std= 9
+larry.subjects= ["Hindi","Physics"]
+print(harry.name, larry.subjects)
+
+
+
+  
+
+   
