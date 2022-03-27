19c56cf210893d6ede11a37e1a5f3970cdb85445
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-03-27 23:37:20 +0530

    Add files via upload

---
+++ tut49.py
@@ -0,0 +1,10 @@
+#Else with for loops
+Khana=["Roti","Sabji","Chawal"]
+for item in Khana:
+    if item=="Burger":
+        print(item)
+    # print(item)
+        break
+else:
+
+    print("Your food is not found")
