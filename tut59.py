4595dc41fe0e446a52e478bb816f3f7fd3038a98
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2022-04-06 23:21:18 +0530

    Add files via upload

---
+++ tut59.py
@@ -0,0 +1,15 @@
+#Raise exception
+#if you want to make sure error before run thhe program then use it
+# a=input("What is your name")
+# b=input("How mauch do you earn")
+# if int(b)==0:
+# #     raise ZeroDivisionError("b is 0 so stoppin the program")
+# # if a.isnumeric():
+#     # raise Exception("Numbers are not allowed")
+# c=input("Enter your name")
+# try:
+#     print(a)
+# except Exception as e:
+#     if c=="harry":
+#         raise ValueError("Harry is blocked")
+#     print("Exception handled")
---
+++ tut60.py
@@ -0,0 +1,20 @@
+# "IS" vs "=="
+a=[3,4,"34"]
+b=[6,4,"34"]
+print(b)
+# if a is b:
+#     print("equal")
+# else:
+#     print("not equal")
+# a=[6,4,"34"]
+c=[0,1,2,3]
+a=c
+
+print(a)
+# if a is b:
+#     print("equal")
+# else:
+#     print("not equal")
+
+
+    
